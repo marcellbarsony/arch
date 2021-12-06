1d5eb6af35301e89ee666b65c523ad4986f3b797
Marcell Barsony <marcellbarsony@protonmail.com>
2021-12-06 03:33:34 +0100

    Removed Suckless software

--- source/suckless.sh
+++ source/suckless.sh
@@ -5,37 +5,37 @@ echo "# Suckless"
 echo "------------------------------"
 echo
 
-echo "Cloning 'dwm' repository"
-echo
-git clone https://github.com/marcellbarsony/dwm.git $HOME/.local/src/dwm
-echo
+#echo "Cloning 'dwm' repository"
+#echo
+#git clone https://github.com/marcellbarsony/dwm.git $HOME/.local/src/dwm
+#echo
 
-echo "Cloning 'st' repository"
-echo
-git clone https://github.com/marcellbarsony/st.git $HOME/.local/src/st
-echo
+#echo "Cloning 'st' repository"
+#echo
+#git clone https://github.com/marcellbarsony/st.git $HOME/.local/src/st
+#echo
 
 echo "Cloning 'dmenu' repository"
 echo
 git clone https://github.com/marcellbarsony/dmenu.git $HOME/.local/src/dmenu
 echo
 
-echo "Cloning 'slstatus' repository"
-echo
-git clone https://github.com/marcellbarsony/slstatus.git $HOME/.local/src/slstatus
-clear
+#echo "Cloning 'slstatus' repository"
+#echo
+#git clone https://github.com/marcellbarsony/slstatus.git $HOME/.local/src/slstatus
+#clear
 
-echo "Changing directory to ~/.local/src/dwm & compiling"
-echo
-cd $HOME/.local/src/dwm
-sudo make clean install
-clear
+#echo "Changing directory to ~/.local/src/dwm & compiling"
+#echo
+#cd $HOME/.local/src/dwm
+#sudo make clean install
+#clear
 
-echo "Changing directory to ~/.local/src/st & compiling"
-echo
-cd $HOME/.local/src/st
-sudo make clean install
-clear
+#echo "Changing directory to ~/.local/src/st & compiling"
+#echo
+#cd $HOME/.local/src/st
+#sudo make clean install
+#clear
 
 echo "Changing directory to ~/.local/src/dmenu & compiling"
 echo
@@ -43,8 +43,8 @@ cd $HOME/.local/src/dmenu
 sudo make clean install
 clear
 
-echo "Changing directory to ~/.local/src/slstatus & compiling"
-echo
-cd $HOME/.local/src/slstatus
-sudo make clean install
-clear
+#echo "Changing directory to ~/.local/src/slstatus & compiling"
+#echo
+#cd $HOME/.local/src/slstatus
+#sudo make clean install
+#clear
