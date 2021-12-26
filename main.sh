ce680096d8b3d20144b29c6557481cd048f7d575
Marcell Barsony <marcellbarsony@protonmail.com>
2021-12-26 19:47:27 +0100

    Created main.sh

---
+++ main.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+while (( "$#" ));
+do
+	case ${1} in
+		--help)
+			echo "------"
+			echo "Arch installation script"
+			echo "------"
+			exit 0
+    ;;
+    --info)
+      echo "Author: Marcell Barsony"
+      echo "Important note: This script is under development"
+      exit 0
+	esac
+	shift
+done
+
