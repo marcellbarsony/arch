8369228649839b4b4c05030891f0c21212eefdea
Marcell Barsony <marcellbarsony@protonmail.com>
2021-12-26 21:44:34 +0100

    Experimenting with tailwhip

--- main.sh
+++ main.sh
@@ -10,7 +10,8 @@ mainmenu(){
 	options+=("Language")
   options+=("Cancel")
 	options+=("Shutdown")
-	select=$(whiptail --title "Arch Install Script" --menu "Select your option" 25 78 16 "${options[@]}" 3>&1 1>&2 2>&3)
+  whiptail --title "Arch Install Script" --menu "Select your option" 25 78 16 "${options[@]}" 3>&1 1>&2 2>&3
+	#select=$(whiptail --title "Arch Install Script" --menu "Select your option" 25 78 16 "${options[@]}" 3>&1 1>&2 2>&3)
 	if [ "$?" = "0" ]; then
 		case ${select} in
 			"Language")
