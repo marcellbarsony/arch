12d4de8681be792fa4db9da104ff62ffdabaf3f5
Marcell Barsony <marcellbarsony@protonmail.com>
2021-12-26 20:14:09 +0100

    Added mainmenu

--- main.sh
+++ main.sh
@@ -1,5 +1,35 @@
 #!/bin/bash
 
+mainmenu(){
+	if [ "${1}" = "" ]; then
+		nextitem="."
+	else
+		nextitem=${1}
+	fi
+	options=()
+	options+=("Language" "English")
+	options+=("Shutdown" "-s")
+	sel=$(whiptail --backtitle "Back title" --title "Arch Install Script" --menu "" --cancel-button "Cancel" --default-item "${nextitem}" 0 0 0 \
+		"${options[@]}" \
+		3>&1 1>&2 2>&3)
+	if [ "$?" = "0" ]; then
+		case ${sel} in
+			"Language")
+				echo "Select language"
+				nextitem="Shutdown"
+			;;
+			"Shutdown")
+				shutdown now
+				nextitem="Language"
+			;;
+		esac
+		mainmenu "${nextitem}"
+	else
+		clear
+	fi
+
+}
+
 while (( "$#" ));
 do
 	case ${1} in
@@ -16,4 +46,6 @@ do
 	esac
 	shift
 done
+
+mainmenu
 
