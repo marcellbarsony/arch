ef19560b4a4267f7b39db8b6f74fddf0d0983770
marcellbarsony <barsonymarcell@gmail.com>
2022-01-02 04:33:06 +0100

    Refactoring main.sh

--- main.sh
+++ main.sh
@@ -35,6 +35,9 @@ note(){
     255)
       echo "Esc pressed."
       ;;
+    *)
+      echo "Exit status $?"
+      ;;
   esac
 }
 
@@ -45,14 +48,24 @@ keyboardlayout(){
     do
       options+=("${item}" "---")
     done
-  keymap=$(dialog --title "Keymap" --menu "menu" 25 78 17 ${options[@]} 3>&1 1>&2 2>&3)
-  if [ "$?" = "0" ]
-    then
-      echo $keymap
+  keymap=$(dialog --title "Keymap" --menu "menu" 20 78 10 ${options[@]} 3>&1 1>&2 2>&3)
+  case $? in
+    0)
+      echo "Slected keymap: $keymap"
       #localectl set-keymap --no-convert $keymap
-      diskselect
-  fi
+      ;;
+    1)
+      echo "CANCEL pressed."
+      ;;
+    255)
+      echo "ESC pressed."
+      ;;
+    *)
+      echo "Exit status $?"
+      ;;
+  esac
 }
+
 diskselect(){
   options=()
   items=$(lsblk -p -n -l -o NAME,SIZE -e 7,11)
@@ -72,9 +85,9 @@ diskselect(){
     return 0
 }
 
-diskpart(){
-  device=$(diskselect "(GPT, EFI)"
-}
+#diskpartmenu(){
+#  device=$(diskselect "(GPT, EFI)"
+#}
 
 # -------------------------------
 # -------------------------------
@@ -83,17 +96,17 @@ while (( "$#" ));
 do
 	case ${1} in
 		--help)
-			echo "------"
-			echo "Arch installation script"
-			echo "------"
-			exit 0
+      echo "------"
+      echo "Arch installation script"
+      echo "------"
+      exit 0
     ;;
     --info)
       echo "Author: Marcell Barsony"
       echo "Important note: This script is under development"
       exit 0
-	esac
-	shift
+  esac
+  shift
 done
 
 dependencies
