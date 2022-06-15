afd874f0da64f3334e4ef6e56f247a93175ee4a9
marci <barsonymarcell@gmail.com>
2022-06-15 02:22:45 +0200

    Backing up script

--- src/chroot.sh
+++ src/chroot.sh
@@ -40,7 +40,7 @@ rootpassword(){
 
 useraccount(){
 
-  username=$(whiptail --inputbox "" --title "User account" --nocancel 8 39 3>&1 1>&2 2>&3)
+  username=$(whiptail --inputbox "" --title "USER ACCOUNT" --nocancel 8 39 3>&1 1>&2 2>&3)
 
   if [ ! ${username} ] || [ ${username} == "root" ]; then
     whiptail --title "ERROR" --msgbox "Username cannot be empty or [root]." 8 78
@@ -54,9 +54,9 @@ useraccount(){
 
 userpassword(){
 
-  password=$(whiptail --passwordbox "User password [${username}]" --title "USER" --nocancel 8 78 3>&1 1>&2 2>&3)
+  password=$(whiptail --passwordbox "User password [${username}]" --title "USER PASSWORD" --nocancel 8 78 3>&1 1>&2 2>&3)
 
-  password_confirm=$(whiptail --passwordbox "User password confirm [${username}]" --title "USER" --nocancel 8 78 3>&1 1>&2 2>&3)
+  password_confirm=$(whiptail --passwordbox "User password confirm [${username}]" --title "USER PASSWORD" --nocancel 8 78 3>&1 1>&2 2>&3)
 
   if [ ! ${password} ] || [ ! ${password_confirm} ]; then
       whiptail --title "ERROR" --msgbox "User password empty." 8 78
@@ -203,13 +203,17 @@ grub(){
 
   # LVM support
   pacman -Qi lvm2 > /dev/null
-  if [ "$?" != "0" ]; then
+  if [ "$?" == "0" ]; then
 
     sed -i /GRUB_CMDLINE_LINUX_DEFAULT=/c\GRUB_CMDLINE_LINUX_DEFAULT=\"cryptdevice=/dev/nvme0n1p3:volgroup0:allow-discards\ loglevel=3\ quiet\ video=1920x1080\" /etc/default/grub
 
     sed -i '/#GRUB_ENABLE_CRYPTODISK=y/s/^#//g' /etc/default/grub
 
   fi
+
+  # GRUB Background
+  # GRUB Theme
+  # GRUB Menu colors
 
   # GRUB - Generate config
   grub-mkconfig -o /boot/grub/grub.cfg
