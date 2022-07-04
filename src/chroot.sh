a27680fb31265cd5c317227a5cd45d8838a993aa
marcellbarsony <barsonymarcell@gmail.com>
2022-07-04 13:40:09 +0200

    Backing up changes

--- src/chroot.sh
+++ src/chroot.sh
@@ -325,8 +325,11 @@ grub()(
 
     luksuuid=$( blkid | grep /dev/sda2 | cut -d\" -f 2 | sed -e 's/-//g' )
 
-    echo "#!/bin/sh" > /etc/grub.d/01_header &>/dev/null
-    echo "echo "cryptomount -u ${luksuuid}"" >> /etc/grub.d/01_header &>/dev/null
+    echo '#!/bin/sh' > /etc/grub.d/01_header &>/dev/null
+    echo -n 'echo "' >> /etc/grub.d/01_header
+    echo -n `echo "cryptomount -u ${luksuuid}"` >> /etc/grub.d/01_header &>/dev/null
+    echo -n '"' >> /etc/grub.d/01_header
+    #chmod
 
     grub_btrfs
 
@@ -334,8 +337,9 @@ grub()(
 
   grub_btrfs(){
 
-    sed -i '/#GRUB_BTRFS_GRUB_DIRNAME="/boot/grub2"/s/^#//g' /etc/default/grub-btrfs/config
-    sed -i 's/GRUB_BTRFS_GRUB_DIRNAME="/boot/grub2"/GRUB_BTRFS_GRUB_DIRNAME="/efi/grub"/g' /etc/grub-btrfs/config
+    sed -i '/#GRUB_BTRFS_GRUB_DIRNAME=/s/^#//g' /etc/default/grub-btrfs/config
+    #sed -i 's/GRUB_BTRFS_GRUB_DIRNAME="/boot/grub2"/GRUB_BTRFS_GRUB_DIRNAME="/efi/grub"/g' /etc/grub-btrfs/config
+    sed -i 's/boot\/grub2/efi\/grub/g' /etc/default/grub-btrfs/config
     #sed -i "s/GRUB_BTRFS_GRUB_DIRNAME=\"/boot/grub2\"/GRUB_BTRFS_GRUB_DIRNAME=\"/efi/grub\"/g" /etc/grub-btrfs/config
 
     systemctl enable --now grub-btrfs.path
@@ -356,11 +360,11 @@ grub()(
     fi
 
     #ext4
-    pacman -Qi lvm2 > /dev/null
-    if [ "$?" == "0" ]; then
-      sed -i /GRUB_CMDLINE_LINUX_DEFAULT=/c\GRUB_CMDLINE_LINUX_DEFAULT=\"cryptdevice=/dev/nvme0n1p3:volgroup0:allow-discards\ loglevel=3\ quiet\ video=1920x1080\" /etc/default/grub
-      sed -i '/#GRUB_ENABLE_CRYPTODISK=y/s/^#//g' /etc/default/grub
-    fi
+    #pacman -Qi lvm2 > /dev/null
+    #if [ "$?" == "0" ]; then
+      #sed -i /GRUB_CMDLINE_LINUX_DEFAULT=/c\GRUB_CMDLINE_LINUX_DEFAULT=\"cryptdevice=/dev/nvme0n1p3:volgroup0:allow-discards\ loglevel=3\ quiet\ video=1920x1080\" /etc/default/grub
+      #sed -i '/#GRUB_ENABLE_CRYPTODISK=y/s/^#//g' /etc/default/grub
+    #fi
 
     grub_install
 
@@ -371,7 +375,7 @@ grub()(
     efidir="/efi"
     bootdir="/efi"
 
-    grub-install --target=x86_64-efi --bootloader-id=GRUB --efi-directory=${efidir} --boot-direcoty=${bootdir}
+    grub-install --target=x86_64-efi --bootloader-id=GRUB --efi-directory=/efi --boot-direcoty=/efi
     local exitcode=$?
 
     if [ "${exitcode}" != "0" ]; then
@@ -400,7 +404,8 @@ grub()(
 
   grub_config(){
 
-    grub-mkconfig -o /boot/grub/grub.cfg
+    #grub-mkconfig -o /boot/grub/grub.cfg
+    grub-mkconfig -o /efi/grub/grub.cfg
     local exitcode=$?
 
     if [ "${exitcode}" != "0" ]; then
