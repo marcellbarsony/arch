656a4b96ee1f0a28842d50f1b8fdc5b3f4c37b7e
marcellbarsony <barsonymarcell@gmail.com>
2022-07-04 15:19:48 +0200

    Install commands

--- main.sh
+++ main.sh
@@ -1,710 +1,103 @@
 #!/bin/bash
 
-network(){
-
-  echo -n "Checking network..."
-  ping -q -c 3 archlinux.org 2>&1 >/dev/null
-
-  case $? in
-    0)
-      echo "[Connected]"
-      bootmode
-      ;;
-    1)
-      echo "[DISCONNECTED]"
-      exit 1
-      ;;
-    *)
-      echo "[ERROR]"
-      echo "Exit status $?"
-      ;;
-  esac
-
-}
-
-bootmode(){
-
-  echo -n "Checking boot mode..."
-  sleep 1
-  ls /sys/firmware/efi/efivars 2>&1 >/dev/null
-
-  case $? in
-    0)
-      echo "[UEFI]"
-      systemclock
-      ;;
-    1)
-      echo "[BIOS]"
-      read -p "Continue installation? (Y/N)" yn
-      case $yn in
-        [Yy])
-          systemclock
-          ;;
-        [Nn])
-          exit 1
-          ;;
-        * )
-          echo invalid response
-          bootmode
-          ;;
-      esac
-      ;;
-    *)
-      echo "[ERROR]"
-      echo "Exit status $?"
-      echo "https://wiki.archlinux.org/title/installation_guide#Verify_the_boot_mode"
-      ;;
-  esac
-
-}
-
-systemclock(){
-
-  echo -n "Set system time with timedatectl..."
-  sleep 1
-  timedatectl set-ntp true --no-ask-password
-
-  case $? in
-    0)
-      echo "[Done]"
-      dependencies
-      ;;
-    *)
-      echo "\nExit status $?"
-      ;;
-  esac
-
-}
-
-dependencies(){
-
-  echo -n "Installing dependencies..."
-  pacman -Sy --noconfirm libnewt dialog 2>&1 >/dev/null # reflector
-  #pacman -Sy --noconfirm libnewt dialog 2>&1 >/dev/null # reflector
-
-  case $? in
-    0)
-      echo "[Done]"
-      keymap
-      ;;
-    *)
-      echo "[ERROR]"
-      echo "Exit status $?"
-      ;;
-  esac
-
-}
-
-keymap(){
-
-  items=$(localectl list-keymaps)
-  options=()
-  options+=("us" "[Default]")
-    for item in ${items}; do
-      options+=("${item}" "")
-    done
-
-  keymap=$(whiptail --title "Keyboard Layout" --menu "" 30 50 20 "${options[@]}" 3>&1 1>&2 2>&3)
-  #keymap=$(dialog --title "Keymap" --menu "menu" 20 50 10 ${options[@]} 3>&1 1>&2 2>&3)
-
-  if [ "$?" = "0" ]; then
-    clear
-    loadkeys ${keymap}
-    localectl set-keymap --no-convert ${keymap} # Systemd - /etc/vconsole.conf
-    warning
-  fi
-
-}
-
-warning(){
-
-  if (whiptail --title "WARNING" --yesno "All data will be erased - Proceed with the installation?" --defaultno 8 60); then
-      diskselect
-    else
-      echo "Installation terminated"
-      echo "Exit status $?"
-  fi
-
-}
-
-diskselect(){
-
-  options=()
-  items=$(lsblk -p -n -l -o NAME,SIZE -e 7,11)
-  for item in ${items}; do
-    options+=("${item}" "")
-  done
-
-  disk=$(whiptail --title "Diskselect" --menu "Select disk to format" 25 78 17 ${options[@]} 3>&1 1>&2 2>&3)
-
-  if [ "$?" != "0" ]
-    then
-      echo "Exit status $?"
-  fi
-    echo ${disk%%\ *}
-    diskpart
-
-}
-
-diskpart(){
-
-  options=()
-  options+=("fdisk" "")
-  options+=("cfdisk" "")
-  options+=("gdisk" "")
-  #options+=("sgdisk" "") #https://man.archlinux.org/man/sgdisk.8
-
-  sel=$(whiptail --backtitle "${apptitle}" --title "Diskpart" --menu "" 0 0 0 "${options[@]}" 3>&1 1>&2 2>&3)
-
-  if [ "$?" = "0" ]; then
-
-    case ${sel} in
-      "fdisk")
-        clear
-        fdisk ${disk}
-        ;;
-      "cfdisk")
-        clear
-        cfdisk ${disk}
-        ;;
-      "gdisk")
-        clear
-        gdisk ${disk}
-        ;;
-    esac
-
-    diskpartcheck
-
-    else
-
-    case $? in
-      1)
-        echo "Cancel pressed"
-        ;;
-      *)
-        echo "Exit status $?"
-        ;;
-      esac
-
-  fi
-
-}
-
-diskpartcheck(){
-
-  items=$(lsblk -p -n -l -o NAME,SIZE -e 7,11)
-
-  if (whiptail --title "Confirm partitions" --yesno "${items}" 18 78); then
-      diskpartmenu
-    else
-      diskselect
-  fi
-
-}
-
-diskpartmenu(){
-
-  options=()
-  options+=("Physical Machine 1" "[GPT+EFI+LVM on Luks]")
-  options+=("Virtual Machine 1" "[GPT+EFI+No encryption]")
-
-  installscheme=$(whiptail --title "Diskpartmenu" --menu "Partition scheme" 0 0 0 "${options[@]}" 3>&1 1>&2 2>&3)
-
-  if [ "$?" = "0" ]; then
-
-    case ${installscheme} in
-      "Physical Machine 1")
-        pm-1
-        ;;
-      "Virtual Machine 1")
-        vm-1
-        ;;
-    esac
-
-    else
-      case $? in
-        1)
-          echo "Cancel pressed"
-          ;;
-        *)
-          echo "Exit status $?"
-          ;;
-      esac
-
-  fi
-
-}
-
-pm-1()(
-
-  ### PARTITION SCHEME ###
-  # Partition 1 | EFI System Partition | (min. 256MB) | [EFI System] ..... |
-  # Partition 2 | Boot ............... | (min. 512MB) | [Linux Filesystem] |
-  # Partition 3 | Root ............... | ............ | [Linux LVM] ...... |
-
-  fsselect(){
-
-    options=()
-    options+=("ext4" "[Default]")
-    options+=("btrfs" "[-]")
-
-    filesystem=$(whiptail --title "[VM-1] File System" --menu "File system" 25 78 17 ${options[@]} 3>&1 1>&2 2>&3)
-
-    if [ "$?" = "0" ]; then
-
-      efiselect
-
-      else
-
-        case $? in
-          1)
-            echo "Cancel pressed"
-            ;;
-          *)
-            echo "Exit status $?"
-            ;;
-        esac
-
-    fi
-
-  }
-
-  efiselect(){
-
-    options=()
-    items=$(lsblk -p -n -l -o NAME,SIZE -e 7,11)
-    for item in ${items}; do
-      options+=("${item}" "")
-    done
-
-    efidevice=$(whiptail --title "[VM-1] EFI" --menu "EFI partition" 25 78 17 ${options[@]} 3>&1 1>&2 2>&3)
-
-    case $? in
-      0)
-        efifilesystem
-        ;;
-      1)
-        echo "Cancel pressed"
-        ;;
-      *)
-        echo "Exit status $?"
-        ;;
-    esac
-
-  }
-
-  efifilesystem(){
-
-    options=()
-    options+=("FAT32" "[Default]")
-
-    efifilesystem=$(whiptail --title "[PM-1] EFI" --menu "EFI file system" 25 78 17 ${options[@]} 3>&1 1>&2 2>&3)
-
-    if [ "$?" = "0" ]; then
-
-        case ${efifilesystem} in
-          "FAT32")
-            efifs="fat -F32"
-            ;;
-          "ext4")
-            echo "Not recommended"
-            exit 1
-            ;;
-        esac
-
-        efiformat
-
-      else
-
-        case $? in
-          1)
-            echo "Cancel pressed"
-            ;;
-          *)
-            echo "Exit status $?"
-            ;;
-        esac
-
-    fi
-
-  }
-
-  efiformat(){
-
-    echo "Success [efiformat]"
-    #mkfs.${efifs} ${efidevice}
-    local exitcode=$?
-
-    if [ ${exitcode} != "0" ]; then
-        whiptail --title "ERROR" --msgbox "Formatting ${efidevice} to FAT32 unsuccessful.\nExit status: ${exitcode}" 8 78
-        diskselect
-    fi
-
-    bootselect
-
-  }
-
-  bootselect(){
-
-    options=()
-    items=$(lsblk -p -n -l -o NAME,SIZE -e 7,11)
-    for item in ${items}; do
-      options+=("${item}" "")
-    done
-
-    bootdevice=$(whiptail --title "[PM-1] BOOT" --menu "BOOT partition" 25 78 17 ${options[@]} 3>&1 1>&2 2>&3)
-
-    case $? in
-      0)
-        bootformat
-        ;;
-      1)
-        echo "Cancel pressed"
-        ;;
-      *)
-        echo "Exit status $?"
-        ;;
-    esac
-
-  }
-
-  bootformat(){
-
-    echo "Success [bootformat]"
-    #mkfs.${filesystem} ${bootdevice}
-    local exitcode=$?
-
-    if [ ${exitcode} != "0" ]; then
-        whiptail --title "ERROR" --msgbox "Formatting ${bootdevice} to ${filesystem} unsuccessful.\nExit status: ${exitcode}" 8 78
-        diskselect
-    fi
-
-    lvmselect
-
-  }
-
-  lvmselect(){
-
-    options=()
-    items=$(lsblk -p -n -l -o NAME,SIZE -e 7,11)
-    for item in ${items}; do
-      options+=("${item}" "")
-    done
-
-    lvmdevice=$(whiptail --title "[PM-1] LVM" --menu "LVM partition" 25 78 17 ${options[@]} 3>&1 1>&2 2>&3)
-
-    case $? in
-      0)
-        cryptsetup
-        ;;
-      1)
-        echo "Cancel pressed"
-        ;;
-      *)
-        echo "Exit status $?"
-        ;;
-    esac
-
-  }
-
-  cryptpassword(){
-
-
-  }
-
-  cryptsetup(){
-
-  cryptsetup -q --type luks2 luksFormat ${lvmdevice} --key-file /root/luks.key
-
-  }
-
-  cryptopen(){
-
-  cryptsetup open --type luks ${lvmdevice} cryptlvm --key-file /root/luks.key
-
-  }
-
-  fsselect
-
-)
-
-vm-1()(
-
-  fsselect(){
-
-    options=()
-    options+=("ext4" "[Default]")
-    options+=("btrfs" "[-]")
-
-    filesystem=$(whiptail --title "[VM-1] File System" --menu "File system" 25 78 17 ${options[@]} 3>&1 1>&2 2>&3)
-
-    if [ "$?" = "0" ]; then
-
-      efiselect
-
-      else
-
-        case $? in
-          1)
-            echo "Cancel pressed"
-            ;;
-          *)
-            echo "Exit status $?"
-            ;;
-        esac
-
-    fi
-
-  }
-
-  efiselect(){
-
-    options=()
-    items=$(lsblk -p -n -l -o NAME,SIZE -e 7,11)
-    for item in ${items}; do
-      options+=("${item}" "")
-    done
-
-    efidevice=$(whiptail --title "[VM-1] EFI" --menu "EFI partition" 25 78 17 ${options[@]} 3>&1 1>&2 2>&3)
-
-    case $? in
-      0)
-        efifilesystem
-        ;;
-      1)
-        echo "Cancel pressed"
-        ;;
-      *)
-        echo "Exit status $?"
-        ;;
-    esac
-
-  }
-
-  efifilesystem(){
-
-    options=()
-    options+=("FAT32" "[Default]")
-
-    efifilesystem=$(whiptail --title "[VM-1] EFI" --menu "EFI file system" 25 78 17 ${options[@]} 3>&1 1>&2 2>&3)
-
-    if [ "$?" = "0" ]; then
-
-        case ${efifilesystem} in
-          "FAT32")
-            efifs="fat -F32"
-            ;;
-          "ext4")
-            echo "Not recommended"
-            exit 1
-            ;;
-        esac
-
-        efiformat
-
-      else
-
-        case $? in
-          1)
-            echo "Cancel pressed"
-            ;;
-          *)
-            echo "Exit status $?"
-            ;;
-        esac
-
-    fi
-
-
-  }
-
-  efiformat(){
-
-    #echo "Success [efiformat]"
-    mkfs.${efifs} ${efidevice}
-    local exitcode=$?
-
-    if [ ${exitcode} != "0" ]; then
-        whiptail --title "ERROR" --msgbox "Formatting ${efidevice} to FAT32 unsuccessful.\nExit status: ${exitcode}" 8 78
-        diskselect
-    fi
-
-    rootselect
-
-  }
-
-  rootselect(){
-
-    options=()
-    items=$(lsblk -p -n -l -o NAME,SIZE -e 7,11)
-    for item in ${items}; do
-      options+=("${item}" "")
-    done
-
-    rootdevice=$(whiptail --title "[VM-1] ROOT" --menu "ROOT partition" 25 78 17 ${options[@]} 3>&1 1>&2 2>&3)
-
-    case $? in
-      0)
-        rootformat
-        ;;
-      1)
-        echo "Cancel pressed"
-        ;;
-      *)
-        echo "Exit status $?"
-        ;;
-    esac
-
-  }
-
-  rootformat(){
-
-    #echo "Success [rootformat]"
-    mkfs.${filesystem} ${rootdevice}
-    local exitcode=$?
-
-    if [ ${exitcode} != "0" ]; then
-        whiptail --title "ERROR" --msgbox "Formatting ${rootdevice} to ${filesystem} unsuccessful.\nExit status: ${exitcode}" 8 78
-        diskselect
-    fi
-
-    mountefi
-
-  }
-
-  mountefi(){
-
-    #echo "Success [efidir]"
-    mkdir /efi
-    local exitcode=$?
-
-    if [ "${exitcode}" != "0" ]; then
-      whiptail --title "ERROR" --msgbox "EFI directory was not created.\nExit status: ${exitcode}" 8 60
-      diskpartmenu
-    fi
-
-    #echo "Success [efimount]"
-    mount ${efidevice} /efi
-    #mount --mkdir ${efidevice} /efi
-    local exitcode=$?
-
-    if [ "${exitcode}" != "0" ]; then
-      whiptail --title "ERROR" --msgbox "EFI partition was not mounted\nExit status: ${exitcode}" 8 60
-      diskpartmenu
-    fi
-
-    mountroot
-
-  }
-
-  mountroot(){
-
-    #echo "Success [rootmount]"
-    mount ${rootdevice} /mnt
-    local exitcode=$?
-
-    if [ "${exitcode}" != "0" ]; then
-      whiptail --title "ERROR" --msgbox "ROOT partition was not mounted\nExit status: ${exitcode}" 8 60
-      diskpartmenu
-    fi
-
-    fstab
-
-  }
-
-  fsselect
-
-)
-
-fstab(){
-
-  #echo "Success [fstab - dir]"
-  mkdir /mnt/etc/
-  local exitcode=$?
-
-  if [ "${exitcode}" != "0" ]; then
-    whiptail --title "ERROR" --msgbox "fstab directory was not created.\nExit status: ${exitcode}" 8 60
-    diskpartmenu
-  fi
-
-  #echo "Success [fstab - gen]"
-  genfstab -U /mnt >> /mnt/etc/fstab
-  #genfstab -L /mnt >> /mnt/etc/fstab
-  local exitcode=$?
-
-  if [ "${exitcode}" != "0" ]; then
-    whiptail --title "ERROR" --msgbox "fstab config was not generated.\nExit status: ${exitcode}" 8 60
-    diskpartmenu
-  fi
-
-  kernel
-
-}
-
-kernel(){
-
-  if [ "${installscheme}" = "Physical Machine 1" ]; then
-      pacstrap /mnt base linux linux-firmware linux-headers base-devel git vim
-    else
-      pacstrap /mnt base linux linux-firmware linux-headers base-devel git vim virtualbox-guest-utils
-  fi
-
-  if [ "${?}" != "0" ]; then
-    whiptail --title "ERROR" --msgbox "Packages were not installed.\nExit status: ${?}" 8 60
-    diskpartmenu
-  fi
-
-  chroot
-
-}
-
-chroot (){
-
-  arch-chroot /mnt
-
-  if [ "${?}" != "0" ]; then
-    whiptail --title "ERROR" --msgbox "Could not chroot into archiso.\nExit status: ${?}" 8 60
-    diskpartmenu
-  fi
-
-}
-
-# -------------------------------
-# -------------------------------
-# -------------------------------
-
-while (( "$#" )); do
-  case ${1} in
-    --help)
-      echo "------"
-      echo "Arch installation script"
-      echo "------"
-      exit 0
-      ;;
-    --info)
-      echo "Author: Marcell Barsony"
-      echo "Repository: https://github.com/marcellbarsony/arch"
-      echo "Important note: This script is under development"
-      exit 0
-      ;;
-    *)
-      echo "Available options:"
-      echo "Help --help"
-      echo "Info --info"
-  esac
-  shift
-done
-
-clear
-network
-#diskselect
-#diskpartconfirm
-#diskpartmenu
-#diskpartcheck
-#fsselect
-
-# NOTES
-# Secure boot: https://wiki.archlinux.org/title/Unified_Extensible_Firmware_Interface/Secure_Boot
-# Minimal Arch install by ML: https://gist.github.com/mattiaslundberg/8620837
-# PM Encrypted UEFI Arch install: https://gist.github.com/HardenedArray/31915e3d73a4ae45adc0efa9ba458b07
-# VM Encrypted UEFI Arch install: https://gist.github.com/HardenedArray/d5b70681eca1d4e7cfb88df32cc4c7e6
+# Partitioning
+sgdisk -o /dev/sda # Clear partition data
+sgdisk -n 0:0:+1GiB -t 0:ef00 -c 0:efi         /dev/sda #EFI
+sgdisk -n 0:0:0     -t 0:8e00 -c 0:cryptsystem /dev/sda #System
+
+# Cryptsetup
+echo ${cryptphrase} | cryptsetup --type luks2 --cipher aes-xts-plain64 --hash sha512 --key-size 256 --pbkdf pbkdf2 --batch-mode luksFormat /dev/sda2 --key-file=-
+echo ${cryptphrase} | cryptsetup open --type luks2 /dev/sda2 cryptroot --key-file=-
+
+# Root partition
+mkfs.btrfs -L system /dev/mapper/cryptroot
+mount /dev/mapper/cryptroot /mnt
+
+# Btrfs subvolume - create
+btrfs subvolume create /mnt/@
+btrfs subvolume create /mnt/@home
+btrfs subvolume create /mnt/@var
+btrfs subvolume create /mnt/@snapshots
+umount -R /mnt
+
+# Btrfs subvolume - mount
+mount -o noatime,compress=zstd,space_cache=v2,discard=async,subvol=@ /dev/mapper/cryptroot /mnt
+mkdir -p /mnt/{efi,boot,home,var,.snapshots}
+mount -o noatime,compress=zstd,space_cache=v2,discard=async,subvol=@home /dev/mapper/cryptroot /mnt/home
+mount -o noatime,compress=zstd,space_cache=v2,discard=async,subvol=@var /dev/mapper/cryptroot /mnt/var
+mount -o noatime,compress=zstd,space_cache=v2,discard=async,subvol=@snapshots /dev/mapper/cryptroot /mnt/.snapshots
+
+# EFI partition
+mkfs.fat -F32 /dev/sda1
+mount /dev/sda1 /mnt/efi
+
+# Fstab
+mkdir /mnt/etc/
+genfstab -U /mnt >> /mnt/etc/fstab
+
+# Mirrorlist
+reflector --latest 20 --protocol https --connection-timeout 5 --sort rate --save /etc/pacman.d/mirrorlist
+
+# Pacstrap
+pacstrap -C ~/arch/cfg/pacman.conf /mnt linux linux-firmware linux-headers base base-devel git vim libnewt intel-ucode # with custom pacman.conf
+
+# Chroot
+arch-chroot /mnt
+
+# Root passphrase
+echo "root:${root_passphrase}" | chpasswd
+
+# User management
+useradd -m ${username}
+echo "${username}:${user_passphrase}" | chpasswd
+usermod -aG wheel,audio,video,optical,storage ${username}
+
+# Hostname
+hostnamectl set-hostname ${nodename}
+
+# Locale
+sed -i '/#en_US.UTF-8 UTF-8/s/^#//g' /etc/locale.gen # Uncomment the line
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
+locale-gen
+
+# Sudoers
+sed 's/# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/g' /etc/sudoers > /etc/sudoers.new #Uncomment wheel group
+export EDITOR="cp /etc/sudoers.new"
+visudo
+rm /etc/sudoers.new
+
+sed '71 i Defaults:%wheel insults' /etc/sudoers > /etc/sudoers.new # Add insults
+export EDITOR="cp /etc/sudoers.new"
+visudo
+rm /etc/sudoers.new
+
+# Initramfs
+sed -i "s/MODULES=()/MODULES=(btrfs)/g" /etc/mkinitcpio.conf # Add Btrfs module
+sed -i "s/block filesystems/block encrypt lvm2 filesystems/g" /etc/mkinitcpio.conf # Add encrypt
+mkinitcpio -p linux
+
+# GRUB packages
+pacman -S --noconfirm grub efibootmgr dosfstools os-prober mtools
+
+# GRUB passphrase
+grubpass=$(echo -e "${grubpw}\n${grubpw}" | grub-mkpasswd-pbkdf2 | cut -d " " -f7 | tr -d '\n')
+echo "cat << EOF" >> /etc/grub.d/00_header
+echo "set superusers=\"${username}\"" >> /etc/grub.d/00_header
+echo "password_pbkdf2 ${username} ${grubpass}" >> /etc/grub.d/00_header
+echo "EOF" >> /etc/grub.d/00_header
+
+# GRUB Btrfs config
+sed -i '/#GRUB_BTRFS_GRUB_DIRNAME=/s/^#//g' /etc/default/grub-btrfs/config #Uncomment the line
+sed -i 's/boot\/grub2/efi\/grub/g' /etc/default/grub-btrfs/config #Change directory: boot/grub2 >> efi/grub
+systemctl enable --now grub-btrfs.path
+
+# GRUB crypt
+uuid=$( blkid | grep /dev/sda2 | cut -d\" -f 2 ) #Root disk UUID, not cryptroot UUID
+sed -i /GRUB_CMDLINE_LINUX_DEFAULT=/c\GRUB_CMDLINE_LINUX_DEFAULT=\"loglevel=3\ quiet\ cryptdevice=UUID=${uuid}:cryptroot:allow-discards\ root=/dev/mapper/cryptroot\ video=1920x1080\" /etc/default/grub
+sed -i /GRUB_PRELOAD_MODULES=/c\GRUB_PRELOAD_MODULES=\"part_gpt\ part_msdos\ luks2\" /etc/default/grub # Add luks2 to the end of the line
+
+# GRUB install
+grub-install --target=x86_64-efi --bootloader-id=GRUB --efi-directory=/efi --boot-direcoty=/efi
+
+# GRUB config
+grub-mkconfig -o /efi/grub/grub.cfg
