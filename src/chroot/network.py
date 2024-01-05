a9f20e7dd9efc4750814aa2a0a9d492db9c8508e
marcellbarsony <barsonymarcell@protonmail.com>
2024-01-05 20:56:59 +0100

    DNS settings

--- src/chroot/network.py
+++ src/chroot/network.py
@@ -51,26 +51,23 @@ class DomainNameSystem():
     def networkmanager():
         """https://wiki.archlinux.org/title/NetworkManager#Unmanaged_/etc/resolv.conf"""
         conf = "/etc/NetworkManager/conf.d/dns.conf"
-        content = textwrap.dedent(
-            """\
+        content = textwrap.dedent( """\
             [main]
             dns=none
-            """
-            )
+        """ )
         try:
             with open(conf, "w") as f:
                 f.write(content)
-            print("[+] NetworkManager dns conf")
+            print("[+] NetworkManager DNS conf")
         except Exception as err:
-            print("[-] NetworkManager dns conf", err)
+            print("[-] NetworkManager DNS conf", err)
             sys.exit(1)
 
     @staticmethod
     def resolvconf():
         """https://wiki.archlinux.org/title/Domain_name_resolution#Overwriting_of_/etc/resolv.conf"""
         conf = "/etc/resolv.conf"
-        content = textwrap.dedent(
-            """\
+        content = textwrap.dedent( """\
             # Cloudflare
             nameserver 1.1.1.1
             nameserver 1.0.0.1
@@ -78,16 +75,15 @@ class DomainNameSystem():
             nameserver 2606:4700:4700::1001
 
             # Quad9
-            # nameserver 9.9.9.9
-            # nameserver 149.112.112.112
-            # nameserver 2620:fe::fe
-            # nameserver 2620:fe::9
-            """
-            )
+            nameserver 9.9.9.9
+            nameserver 149.112.112.112
+            nameserver 2620:fe::fe
+            nameserver 2620:fe::9
+        """ )
         try:
             with open(conf, "w") as file:
                 file.write(content)
             print("[+] /etc/resolv.conf")
         except Exception as err:
-            print("[+] /etc/resolv.conf", err)
+            print("[-] /etc/resolv.conf", err)
             sys.exit(1)
