b4f9b652d69245b482a142f0e2d1d2ace3adb855
marcellbarsony <barsonymarcell@protonmail.com>
2025-01-17 23:40:11 +0100

    Refactoring main

--- main.py
+++ main.py
@@ -21,87 +21,6 @@ from src.install import install
 from src.install import chroot
 # }}}
 
-# Check {{{
-def run_check():
-    check.boot_mode()
-    check.network(network_ip, network_port)
-# }}}
-
-# Init {{{
-def initialize():
-    init.time_zone(timezone)
-    init.loadkeys(keys)
-    init.keymaps(keymap)
-# }}}
-
-# File system {{{
-def file_system():
-    disk.wipe(device)
-    disk.create_efi(device, efisize)
-    disk.create_system(device)
-    disk.partprobe(device)
-# }}}
-
-# Encryption {{{
-def set_encryption():
-    encrypt.modprobe()
-    encrypt.encrypt(device_root, cryptpassword)
-    encrypt.open(device_root, cryptpassword)
-# }}}
-
-# BTRFS {{{
-def set_btrfs():
-    btrfs.mkfs(rootdir)
-    btrfs.mountfs(rootdir)
-    btrfs.mksubvols()
-    btrfs.unmount()
-    btrfs.mount_root(rootdir)
-    btrfs.mkdir(subvolumes)
-    btrfs.mount_subvolumes(subvolumes, rootdir)
-# }}}
-
-# EFI {{{
-def set_efi():
-    efi.mkdir(efidir)
-    efi.format(device_efi)
-    efi.mount(device_efi, efidir)
-# }}}
-
-# Fstab {{{
-def gen_fstab():
-    fstab.mkdir()
-    fstab.genfstab()
-# }}}
-
-# Pacman {{{
-def packages():
-    pacman.config()
-    pacman.mirrorlist()
-    pacman.keyring_init()
-# }}}
-
-# Pacstrap {{{
-def pacstrap():
-    install.bug()
-
-    pkgs = install.get_pkgs()
-    install.install(pkgs)
-
-    pkgs_dmi = install.get_pkgs_dmi(dmidecode)
-    install.install(pkgs_dmi)
-# }}}
-
-# Chroot {{{
-def arch_chroot():
-    cfg_src = f"{cwd}/config.ini"
-    cfg_dst = "/mnt/config.ini"
-    scr_src = f"{cwd}/src/"
-    scr_dst = "/mnt/temporary"
-    chroot.copy(scr_src, scr_dst, cfg_src, cfg_dst)
-    chroot.chroot()
-    chroot.clear(scr_dst, cfg_dst)
-# }}}
-
 
 if __name__ == "__main__":
 
@@ -109,7 +28,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         prog = "python3 setup.py",
         description = "Arch base system",
-        epilog = "TODO"
+        epilog = "Arch base system setup script"
     )
     args = parser.parse_args()
     # }}}
@@ -121,45 +40,48 @@ if __name__ == "__main__":
     )
     # }}}
 
+    # Global variables {{{
+    user = getpass.getuser()
+    cwd = os.getcwd()
+
+    # DMI table decoder
+    device, device_efi, device_root = dmi.disk()
+    dmidecode = dmi.check()
+    # }}}
+
     # Config {{{
     config = configparser.ConfigParser()
     config.read("config.ini")
 
-    # Auth
+    # Auth {{{
     cryptpassword = config.get("auth", "crypt")
-    auth_1        = config.get("auth", "grub")
-    auth_2        = config.get("auth", "root_pw")
-    auth_3        = config.get("auth", "user")
-    auth_4        = config.get("auth", "user_pw")
+    # }}}
 
-    # Btrfs
+    # Btrfs {{{
     subvolumes    = config.get("btrfs", "subvolumes").split(", ")
-    btfrs_1       = config.get("btrfs", "btrfs_cfg")
+    # }}}
 
-    # Disk
+    # Disk {{{
     efisize       = config.get("disk", "efisize")
     rootdir       = config.get("disk", "rootdir")
     efidir        = config.get("disk", "efidir")
+    # }}}
 
-    # Grub
-    grub_1        = config.get("grub", "efi_directory")
-    grub_2        = config.get("grub", "secureboot")
-
-    # Keyset
+    # Keyset {{{
     font          = config.get("keyset", "font")
     keys          = config.get("keyset", "keys")
     keymap        = config.get("keyset", "keymap")
+    # }}}
 
-    # Network
+    # Network {{{
     network_ip    = config.get("network", "ip")
     network_port  = config.get("network", "port")
-    network_1     = config.get("network", "hostname")
+    network_host  = config.get("network", "hostname")
+    # }}}
 
-    # NextDNS
-    nextdns       = config.get("nextdns", "profile")
-
-    # Timezone
+    # Timezone {{{
     timezone      = config.get("timezone", "zone")
+    # }}}
 
     for section in config.sections():
         for key, value in config.items(section):
@@ -171,24 +93,73 @@ if __name__ == "__main__":
                 raise ValueError(f"Empty value '{key}' in section [{section}]")
     # }}}
 
-    # Global variables {{{
-    user = getpass.getuser()
-    cwd = os.getcwd()
-
-    # DMI table decoder
-    device, device_efi, device_root = dmi.disk()
-    dmidecode = dmi.check()
+    # Run {{{
+    # Check {{{
+    check.boot_mode()
+    check.network(network_ip, network_port)
     # }}}
 
-    # Run {{{
-    run_check()
-    initialize()
-    file_system()
-    set_encryption()
-    set_btrfs()
-    set_efi()
-    gen_fstab()
-    packages()
-    pacstrap()
-    arch_chroot()
+    # Init {{{
+    init.time_zone(timezone)
+    init.loadkeys(keys)
+    init.keymaps(keymap)
+    # }}}
+
+    # File system {{{
+    disk.wipe(device)
+    disk.create_efi(device, efisize)
+    disk.create_system(device)
+    disk.partprobe(device)
+    # }}}
+
+    # Encryption {{{
+    encrypt.modprobe()
+    encrypt.encrypt(device_root, cryptpassword)
+    encrypt.open(device_root, cryptpassword)
+    # }}}
+
+    # BTRFS {{{
+    btrfs.mkfs(rootdir)
+    btrfs.mountfs(rootdir)
+    btrfs.mksubvols()
+    btrfs.unmount()
+    btrfs.mount_root(rootdir)
+    btrfs.mkdir(subvolumes)
+    btrfs.mount_subvolumes(subvolumes, rootdir)
+    # }}}
+
+    # EFI {{{
+    efi.mkdir(efidir)
+    efi.format(device_efi)
+    efi.mount(device_efi, efidir)
+    # }}}
+
+    # Fstab {{{
+    fstab.mkdir()
+    fstab.genfstab()
+    # }}}
+
+    # Pacman {{{
+    pacman.config()
+    pacman.mirrorlist()
+    pacman.keyring_init()
+    # }}}
+
+    # Pacstrap {{{
+    install.bug()
+    pkgs = install.get_pkgs()
+    install.install(pkgs)
+    pkgs_dmi = install.get_pkgs_dmi(dmidecode)
+    install.install(pkgs_dmi)
+    # }}}
+
+    # Chroot {{{
+    cfg_src = f"{cwd}/config.ini"
+    cfg_dst = "/mnt/config.ini"
+    scr_src = f"{cwd}/src/"
+    scr_dst = "/mnt/temporary"
+    chroot.copy(scr_src, scr_dst, cfg_src, cfg_dst)
+    chroot.chroot()
+    chroot.clear(scr_dst, cfg_dst)
+    # }}}
     # }}}
--- src/main.py
+++ src/main.py
@@ -23,106 +23,17 @@ from chroot import users
 from chroot import x11
 # }}}
 
-# Locale {{{
-def set_locale():
-    locale.locale()
-    locale.conf()
-    locale.gen()
-# }}}
-
-# Hosts {{{
-def set_hosts():
-    host.hostname(hostname)
-    host.hosts(hostname)
-# }}}
-
-# Users {{{
-def set_users():
-    users.root_password(root_pw)
-    users.user_add(user)
-    users.user_password(user, user_pw)
-    users.user_group(user)
-# }}}
-
-# Security {{{
-def set_security():
-    security.sudoers()
-    security.login_delay()
-# }}}
-
-# Initramfs (mkinitcpio) {{{
-def set_initramfs():
-    kms = initramfs.kernel_mode_setting()
-    initramfs.initramfs(kms)
-    initramfs.mkinitcpio()
-# }}}
-
-# GRUB {{{
-def set_bootloader():
-    uuid = grub.get_uuid(device_root)
-    grub.setup(uuid)
-    grub.install(secureboot, efi_directory)
-    # grub.password(grub_password, user)
-    grub.mkconfig()
-# }}}
-
-# Systemd {{{
-def set_systemd():
-    systemd.logind()
-    dmi_res = dmi.check()
-    systemd.services(dmi_res)
-    systemd.watchdog()
-    systemd.pc_speaker()
-# }}}
-
-# DNS (DoH) {{{
-def set_dns():
-    dns.networkmanager()
-    dns.resolvconf()
-    dns.doh(nextdns_profile)
-# }}}
-
-# Btrfs {{{
-def set_btrfs():
-    snapper.config_init(btrfs_cfg)
-    snapper.config_set(btrfs_cfg)
-    snapper.systemd_services()
-# }}}
-
-# SSH {{{
-def set_ssh():
-    ssh.bashrc(user)
-# }}}
-
-# Pacman {{{
-def set_pacman():
-    mirrorlist.backup()
-    mirrorlist.update()
-    mirrorlist.systemd()
-    pacman.config()
-# }}}
-
-# X11 {{{
-def set_xorg():
-    x11.keymaps()
-# }}}
-
-# Post script {{{
-def arch_post():
-    post.clone(user)
-    post.chown(user)
-# }}}
-
-# Finalize {{{
-def set_finalize():
-    finalize.change_ownership(user)
-    finalize.remove_xdg_dirs(user)
-# }}}
-
 
 if __name__ == "__main__":
 
-    # Initialize Global variables {{{
+    # Logging {{{
+    logging.basicConfig(
+        level = logging.INFO, filename="logs.log", filemode="w",
+        format = ":: %(levelname)s :: %(module)s - %(funcName)s: %(lineno)d\n%(message)-1s\n"
+    )
+    # }}}
+
+    # Global variables {{{
     config = configparser.ConfigParser()
     config.read("/config.ini")
 
@@ -141,25 +52,86 @@ if __name__ == "__main__":
     device, device_efi, device_root = dmi.disk()
     # }}}
 
-    # Initialize Logging {{{
-    logging.basicConfig(
-        level = logging.INFO, filename="logs.log", filemode="w",
-        format = ":: %(levelname)s :: %(module)s - %(funcName)s: %(lineno)d\n%(message)-1s\n"
-    )
+    # Run {{{
+    # Locale {{{
+    locale.locale()
+    locale.conf()
+    locale.gen()
     # }}}
 
-    # Run script {{{
-    set_locale()
-    set_hosts()
-    set_users()
-    set_security()
-    set_initramfs()
-    set_bootloader()
-    set_systemd()
-    set_dns()
-    set_btrfs()
-    set_ssh()
-    set_pacman()
-    # set_xorg()
-    set_finalize()
+    # Hosts {{{
+    host.hostname(hostname)
+    host.hosts(hostname)
+    # }}}
+
+    # Users {{{
+    users.root_password(root_pw)
+    users.user_add(user)
+    users.user_password(user, user_pw)
+    users.user_group(user)
+    # }}}
+
+    # Security {{{
+    security.sudoers()
+    security.login_delay()
+    # }}}
+
+    # Initramfs (mkinitcpio) {{{
+    kms = initramfs.kernel_mode_setting()
+    initramfs.initramfs(kms)
+    initramfs.mkinitcpio()
+    # }}}
+
+    # GRUB {{{
+    uuid = grub.get_uuid(device_root)
+    grub.setup(uuid)
+    grub.install(secureboot, efi_directory)
+    # grub.password(grub_password, user)
+    grub.mkconfig()
+    # }}}
+
+    # Systemd {{{
+    systemd.logind()
+    dmi_res = dmi.check()
+    systemd.services(dmi_res)
+    systemd.watchdog()
+    systemd.pc_speaker()
+    # }}}
+
+    # DNS (DoH) {{{
+    dns.networkmanager()
+    dns.resolvconf()
+    dns.doh(nextdns_profile)
+    # }}}
+
+    # Btrfs {{{
+    snapper.config_init(btrfs_cfg)
+    snapper.config_set(btrfs_cfg)
+    snapper.systemd_services()
+    # }}}
+
+    # SSH {{{
+    ssh.bashrc(user)
+    # }}}
+
+    # Pacman {{{
+    mirrorlist.backup()
+    mirrorlist.update()
+    mirrorlist.systemd()
+    pacman.config()
+    # }}}
+
+    # X11 {{{
+    x11.keymaps()
+    # }}}
+
+    # Post script {{{
+    post.clone(user)
+    post.chown(user)
+    # }}}
+
+    # Finalize {{{
+    finalize.change_ownership(user)
+    finalize.remove_xdg_dirs(user)
+    # }}}
     # }}}
